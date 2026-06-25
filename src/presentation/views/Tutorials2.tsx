aadc9f1799cd8434f1945827abdbfba1def255f0
Luiz Felipe <luizfmsilva29082005@gmail.com>
2026-06-25 17:29:16 -0300

    Paginas de Tutorial - sem styles

---
+++ src/presentation/views/Tutorials2.tsx
@@ -0,0 +1,108 @@
+import React, { useState } from "react";
+import { COLORS } from './../theme/AppTheme';
+import {
+  View, Text, Image, StyleSheet, ToastAndroid, Platform, TouchableOpacity,
+} from "react-native";
+
+export const Tutorials2 = () => {
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Respostas do Olhuz</Text>
+      <View style={styles.card}>
+        <Image style={styles.img} source={require("../../../assets/img/Group 90.png")} />
+        <Text style={styles.leg}>Voz: lê o que está na tela
+Som: alerta e ações</Text>
+      </View>
+      
+      <View style={styles.container}>   
+        <Image style={styles.img} source={require("../../../assets/img/Group.png")}/>
+        <Text style={styles.leg}>Vibração: confirma escolhas</Text>
+      </View>
+      <View>
+      <TouchableOpacity style={styles.btnUp}>Pular</TouchableOpacity>
+      <TouchableOpacity style={styles.btnRepeat}>Repetir</TouchableOpacity>    
+      <TouchableOpacity style={styles.btAdvance}>Avançar</TouchableOpacity>
+      </View>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingHorizontal: 20,
+    marginVertical: 15,
+  
+  },
+  title: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "#000066", 
+    textAlign: "center",
+    marginBottom: 50,
+    paddingBottom:20,
+  },
+  card: {
+    alignItems: "center",
+    justifyContent: "center",
+    width: "100%",
+  },
+  leg: {
+    maxWidth: "80%",
+    fontSize: 24,
+    fontWeight: "600",
+    color: "#000000",
+    textAlign: "center",
+    marginTop: 15,
+    lineHeight: 24,
+  },
+  img: {
+    width: 190,
+    height: 190,
+    resizeMode: "contain",
+    
+   },
+  btnUp: {
+    backgroundColor: "#E0E0E0", 
+    width: "300%",
+    height: 50,
+    fontSize: 22,
+    borderRadius: 25,
+    justifyContent:"center",
+    alignItems: "center",
+    alignSelf: "flex-end",
+    marginLeft: "4%",
+    
+  },
+  btnRepeat: {
+    backgroundColor: "#D94B06", 
+    color: COLORS.white,
+    width: "300%",
+    height: 50,
+    fontSize: 22,
+    borderRadius: 25,
+    justifyContent:"center",
+    alignItems: "center",
+    alignSelf: "flex-start",
+    marginRight: "4%",
+    marginTop: -50, 
+  
+  },
+  btAdvance: {
+    backgroundColor: "#000099",  
+    color: COLORS.white,
+    width: "300%",
+    height: 50,
+    fontSize: 22,
+    borderRadius: 25,
+    justifyContent:"center",
+    alignItems: "center",
+    alignSelf: "center",
+    marginTop: 25,
+  },
+});
+
+export default Tutorials2;
